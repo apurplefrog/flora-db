7ac8c0c9fb73094ff0bdd393316790c72cb5f481
autumn-the-kitty-cat <theodorebaker08@gmail.com>
2025-02-11 14:24:39 -0500

    restart init

--- src/main.rs
+++ src/main.rs
@@ -5,72 +5,54 @@
  * D: Durability (stored on disk)
  */
 
-use std::collections::HashMap;
-
-#[derive(Debug)]
 struct Database {
-    tables: Option<HashMap<String, Table>>,
+    changes: Vec<DatabaseChange>,
 }
 
 impl Database {
     fn new() -> Self {
-        Self { tables: None }
+        Self { changes: vec![] }
     }
-
-    fn add_table(&mut self, name: String) {
-        self.tables = Some(HashMap::from([(name, Table {
-            rows: None,
-            cols: None,
-            data: None,
-        })]));
+    fn create_table(&mut self, name: TableName) {
+        todo!();
     }
-
-    fn get_mut_tables(&mut self) -> &mut Option<HashMap<String, Table>> {
-        &mut self.tables
+    fn commit(&mut self) {
+        todo!();
     }
 }
 
-#[derive(Debug)]
-struct Table {
-    rows: Option<Vec<String>>,
-    cols: Option<Vec<String>>,
-    data: Option<Vec<Vec<Cell>>>,
+struct TableName(String);
+
+enum DatabaseChange {
+    CreateTable(TableName),
+    RemoveTable(TableName),
+    EditTable(TableName, Vec<TableChange>),
 }
 
-impl Table {
-    fn add_row(&mut self, row_name: String) {
-        self.rows.as_mut().unwrap().push(row_name);
-    }
-}
-
-#[derive(Debug)]
-enum Cell {
+enum CellType {
     String(String),
-    Integer(i32),
-    Float(f32),
+    Integer(i64),
+    Float(f64),
     None,
 }
 
+struct ColumnName;
+
+struct RowName;
+struct Row {
+    contents: Vec<CellType>,
+}
+
+enum TableChange {
+    AddColumn(ColumnName),
+    RemoveColumn(ColumnName),
+    AddRow(RowName, Row),
+    RemoveRow(RowName, Row),
+    EditCell(ColumnName, RowName, CellType),
+}
+
 fn main() {
-    // Create database
     let mut database = Database::new();
-
-    // Add table People
-    database.add_table("People".into());
-
-    // Add columns [name, age, id]
-    let mut tables = database.get_mut_tables().as_mut().unwrap();
-    tables.insert("People".into(), Table {
-        rows: Some(vec!["name".into(), "age".into(), "id".into()]),
-        cols: None,
-        data: None,
-    });
-
-    // Add John to people with age 27 and id 0
-    // Add Mary to people with age 28 and id 1
-    // Get John's age
-    // Set John's age to 28
-    // Get John's age (should err)
-    // Commit John's age
-    // Get John's age
+    database.create_table(TableName("YAY".into()));
+    database.commit();
 }
